cfc9fada47fce0ba2b7beba4d69760b110d2155e
Peter Houghton <pete@investigatingsoftware.co.uk>
2023-04-08 12:32:24 +0000

    Add all buttons and basic interaction for initial stages

--- playing-cards-app/src/App.js
+++ playing-cards-app/src/App.js
@@ -1,50 +1,69 @@
 import React, { useState } from 'react';
 import './App.css';
 
-// Define the suits and ranks arrays
 const suits = ['♥', '♦', '♣', '♠'];
 const ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
 
 function App() {
-  // State to hold the selected suit and rank, choices, summary, and test area results
   const [selectedSuit, setSelectedSuit] = useState('');
   const [selectedRank, setSelectedRank] = useState('');
   const [choices, setChoices] = useState('');
   const [summary, setSummary] = useState('');
-  const [testAreaResults, setTestAreaResults] = useState('');
+  const [resultsSummary, setResultsSummary] = useState('');
+  const [score, setScore] = useState('');
+  const [isCrib, setIsCrib] = useState(false);
 
-  // Function to handle suit button click
   const handleSuitButtonClick = (suit) => {
     setSelectedSuit(suit);
     setChoices(`${selectedRank} ${suit}`);
   };
 
-  // Function to handle rank button click
   const handleRankButtonClick = (rank) => {
     setSelectedRank(rank);
     setChoices(`${rank} ${selectedSuit}`);
   };
 
-  // Function to clear choices text box
   const clearChoices = () => {
     setSelectedSuit('');
     setSelectedRank('');
     setChoices('');
   };
 
-  // Function to add the chosen suit and rank to the summary text box
   const addToSummary = () => {
     setSummary(`${summary}${choices}\n`);
   };
 
-  // Function to handle submit button click
+  const clearSummary = () => {
+    setSummary('');
+  };
+
   const handleSubmit = () => {
-    setTestAreaResults(summary);
+    setResultsSummary(summary);
+  };
+
+  const makeStartCard = () => {
+    setChoices(`Start: ${choices}`);
+    setSummary(`Start: ${summary}${choices}\n`);
+  };
+
+  const clearAll = () => {
+    clearChoices();
+    clearSummary();
+    setResultsSummary('');
+    setScore('');
   };
 
   return (
     <div className="App">
       <h1>Playing Cards</h1>
+      <h2>Select a rank:</h2>
+      <div className="ranks">
+        {ranks.map((rank) => (
+          <button key={rank} onClick={() => handleRankButtonClick(rank)}>
+            {rank}
+          </button>
+        ))}
+      </div>
       <h2>Select a suit:</h2>
       <div className="suits">
         {suits.map((suit) => (
@@ -57,31 +76,54 @@ function App() {
           </button>
         ))}
       </div>
-      <h2>Select a rank:</h2>
-      <div className="ranks">
-        {ranks.map((rank) => (
-          <button key={rank} onClick={() => handleRankButtonClick(rank)}>
-            {rank}
-          </button>
-        ))}
+      <div className="section-container">
+        <h2>Chosen Card:</h2>
+        <div className="choices">
+          <textarea value={choices} readOnly rows="3" cols="12" className="big-text" />
+          <div className="choices-buttons">
+            <button onClick={clearChoices}>Clear</button>
+            <button onClick={addToSummary}>Add</button>
+            <button onClick={makeStartCard}>Make 'Start' Card</button>
+          </div>
+        </div>
       </div>
-      <div>
-        <h2>Chosen Suit and Rank:</h2>
-        <textarea value={choices} readOnly />
-        <button onClick={clearChoices}>Clear</button>
-        <button onClick={addToSummary}>Add</button>
+      <div className="section-container">
+        <h2>Your Hand of Cards:</h2>
+        <div className="summary">
+          <textarea value={summary} readOnly rows="6" className="big-text" />
+          <div className="summary-buttons">
+            <button onClick={clearSummary}>Clear</button>
+            <button onClick={handleSubmit}>Submit</button>
+          </div>
+        </div>
+        <div className="crib-checkbox">
+          <input
+              type="checkbox"
+              checked={isCrib}
+              onChange={() => setIsCrib(!isCrib)}
+            />
+            <label>Crib</label>
+          </div>
+        </div>
+        <div className="section-container">
+          <h2>Results Summary:</h2>
+          <textarea value={resultsSummary} readOnly rows="8" cols="25" className="big-text" />
+        </div>
+        <div className="section-container">
+          <h2>Score:</h2>
+          <input
+            value={score}
+            onChange={(e) => setScore(e.target.value)}
+            className="big-text"
+            style={{ fontSize: '1.5rem', width: '8rem' }}
+          />
+        </div>
+        <div className="section-container">
+          <button onClick={clearAll}>Clear All</button>
+        </div>
       </div>
-      <div>
-        <h2>Summary:</h2>
-        <textarea value={summary} readOnly />
-        <button onClick={handleSubmit}>Submit</button>
-      </div>
-      <div>
-        <h2>Test Area:</h2>
-        <textarea value={testAreaResults} readOnly />
-      </div>
-    </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
